98620e58ad3c33f6919a4a280f628c16801cfe19
jsbyun3 <jsbyun3@kmu.kr>
2026-02-18 15:31:39 +0000

    Deploying to gh-pages from @ jsbyun3/IN-SBLab@2a5f93cb1f875b6b37b325c48ac443f9a16a5079 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,12 +30,12 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/IN-SBLab/projects/";
           },
-        },{id: "nav-repositories",
-          title: "repositories",
+        },{id: "nav-software",
+          title: "software",
           description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/IN-SBLab/repositories/";
+            window.location.href = "/IN-SBLab/software/";
           },
         },{id: "nav-teaching",
           title: "teaching",
