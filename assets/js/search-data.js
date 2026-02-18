a0687a72229fceaf712e79146f4f56bf10c47f76
jsbyun3 <jsbyun3@kmu.kr>
2026-02-18 15:35:54 +0000

    Deploying to gh-pages from @ jsbyun3/IN-SBLab@3cbd6fcce4c1e23919f646c7886e4d1fa61e8afb 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,7 +9,14 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/IN-SBLab/";
     },
-  },{id: "nav-news",
+  },{id: "nav-people",
+          title: "people",
+          description: "members of the lab or group",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/IN-SBLab/people/";
+          },
+        },{id: "nav-news",
           title: "news",
           description: "",
           section: "Navigation",
@@ -43,13 +50,6 @@ ninja.data = [{
           section: "Navigation",
           handler: () => {
             window.location.href = "/IN-SBLab/teaching/";
-          },
-        },{id: "nav-people",
-          title: "people",
-          description: "members of the lab or group",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/IN-SBLab/people/";
           },
         },{id: "dropdown-bookshelf",
               title: "bookshelf",
